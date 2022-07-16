dc1b4e0767291650547bce404ea0d30f9948221e
SimonZhan-code <simonzhan@berkeley.edu>
2022-07-15 19:39:24 -0700

    Adding the handelman option to the LP formulation process

---
+++ Handelman_Relaxation/Handelman_Generation.py
@@ -0,0 +1,49 @@
+from scipy.interpolate import BPoly
+import cvxpy as cp
+import numpy as np
+import numpy.random as npr
+import scipy.sparse as sp
+from scipy import special
+import torch
+import scipy
+import cvxpylayers
+from cvxpylayers.torch.cvxpylayer import CvxpyLayer
+import matplotlib.pyplot as plt
+from sympy import MatrixSymbol, Matrix
+from itertools import *
+# from sympy.solvers import
+from sympy import *
+import matplotlib.patches as mpatches
+
+
+def possible_polynomial_generation(deg, Poly):
+	p = []
+	dim = len(Poly)
+	I = []
+	# Possible I generation
+	arr_comb = []
+	for i in range(deg+1):
+		arr_comb.append(i)
+	# Get all possible selection
+	I_temp_comb = list(combinations_with_replacement(arr_comb, dim))
+	I_temp = []
+	# Get all possible permutation
+	for i in I_temp_comb:
+		I_temp_permut = list(permutations(i, dim))
+		I_temp += I_temp_permut
+	# Deduce the redundant option and exceeding power terms
+	[I.append(x) for x in I_temp if x not in I and sum(x) <= deg]
+	# Generate possible terms from the I given
+	for i in I:
+		monomial = 1
+		for j in range(len(i)):
+			monomial = monomial*Poly[j]**i[j]
+		p.append(monomial)
+	return p
+
+possible_polynomial_generation(2, [1,2])
+
+
+
+
+
