3c11dd218b3efe64cfed5d5dfba1ca0865c7049f
SimonZhan-code <simonzhan@berkeley.edu>
2022-07-17 19:18:33 -0700

    handelman experiment for exmaple 6.1

--- Handelman_Relaxation/Handelman_Generation.py
+++ Handelman_Relaxation/Handelman_Generation.py
@@ -74,7 +74,7 @@ def GetDerivative(dynamics, polymonial_terms, X):
 			temp[i] = diff(m, X[i]) * dynamics[i]
 		temp_der = sum(temp)
 		ele_der.append(expand(temp_der))
-	return ele_der
+	return Matrix(ele_der)
 
 
 def generateConstraints(exp1, exp2, degree, constraints):
@@ -82,11 +82,15 @@ def generateConstraints(exp1, exp2, degree, constraints):
 		for i in range(degree+1):
 			for j in range(degree+1):
 				if i + j <= degree:
-					constraints += [exp1.coeff(x, i).coeff(y, j) == exp2.coeff(x, i).coeff(y, j)]
+					print('constraints += [', exp1.coeff(x, i).coeff(y, j), ' == ', exp2.coeff(x, i).coeff(y, j), ']')
 
 
-
-def Lyapunov_func_encoding(deg, Poly, dynamics, X, alpha, max_deg, poly_list, monomial_list):
+def Lyapunov_func_encoding(deg, Poly, dynamics, X, alpha, max_deg):
+	# Return constraints of the LP question
+	# Generate the possible polynomial power product
+	poly_list = possible_polynomial_generation(max_deg, Poly)
+	# Generate the possible monomial power product 
+	monomial_list = monomial_generation(deg, X)
 	# Coefficients of polynomial power product
 	lambda_poly_init = MatrixSymbol('lambda_1', 1, len(poly_list))
 	lambda_poly_der = MatrixSymbol('lambda_2', 1, len(poly_list))	
@@ -103,6 +107,9 @@ def Lyapunov_func_encoding(deg, Poly, dynamics, X, alpha, max_deg, poly_list, mo
 		rhs_init = rhs_init + alpha*x**2
 	
 	generateConstraints(rhs_init, lhs_init, deg, constraints)
+	print("")
+	print("")
+	print("")
 
 	# Encode the lie derivative of Lyapunov function
 	# poly_der = GetDerivative(dynamics, poly_list, X)
@@ -112,7 +119,7 @@ def Lyapunov_func_encoding(deg, Poly, dynamics, X, alpha, max_deg, poly_list, mo
 	lhs_der = c * monomial_der
 	lhs_der = expand(lhs_der[0,0])
 	rhs_der = -lambda_poly_der * poly_list
-	rhs_der = expand(rhs_der)
+	rhs_der = expand(rhs_der[0,0])
 	for temp in square_der:
 		rhs_der += temp
 	
@@ -121,18 +128,42 @@ def Lyapunov_func_encoding(deg, Poly, dynamics, X, alpha, max_deg, poly_list, mo
 	return constraints
 
 
-def FindLyapunov(deg, Poly, dynamics, X, alpha, max_deg):
+def FindLyapunov(deg, Poly, X, max_deg):
 	# Return constraints of the LP question
 	# Generate the possible polynomial power product
 	poly_list = possible_polynomial_generation(max_deg, Poly)
 	# Generate the possible monomial power product 
 	monomial_list = monomial_generation(deg, X)
-	c = cp.Variable((1, len(poly_list)))
-	lambda_1 = cp.Variable((1, len(monomial_list)), pos = True)
-	lambda_2 = cp.Variable((1, len(monomial_list)), pos = True)
+	c = cp.Variable((1, len(monomial_list)))
+	lambda_1 = cp.Variable((1, len(poly_list)), pos = True)
+	lambda_2 = cp.Variable((1, len(poly_list)), pos = True)
 	objective = cp.Minimize(0)
+	
+	constraints = []
+	constraints += [ lambda_1[0, 0] + lambda_1[0, 1] + lambda_1[0, 2] + lambda_1[0, 3] + lambda_1[0, 4] + lambda_1[0, 5] + lambda_1[0, 6] + lambda_1[0, 7] + lambda_1[0, 8] + lambda_1[0, 9] + lambda_1[0, 10] + lambda_1[0, 11] + lambda_1[0, 12] + lambda_1[0, 13] + lambda_1[0, 14] + lambda_1[0, 15] + lambda_1[0, 16] + lambda_1[0, 17] + lambda_1[0, 18] + lambda_1[0, 19] + lambda_1[0, 20] + lambda_1[0, 21] + lambda_1[0, 22] + lambda_1[0, 23] + lambda_1[0, 24] + lambda_1[0, 25] + lambda_1[0, 26] + lambda_1[0, 27] + lambda_1[0, 28] + lambda_1[0, 29] + lambda_1[0, 30] + lambda_1[0, 31] + lambda_1[0, 32] + lambda_1[0, 33] + lambda_1[0, 34] + lambda_1[0, 35] + lambda_1[0, 36] + lambda_1[0, 37] + lambda_1[0, 38] + lambda_1[0, 39] + lambda_1[0, 40] + lambda_1[0, 41] + lambda_1[0, 42] + lambda_1[0, 43] + lambda_1[0, 44] + lambda_1[0, 45] + lambda_1[0, 46] + lambda_1[0, 47] + lambda_1[0, 48] + lambda_1[0, 49] + lambda_1[0, 50] + lambda_1[0, 51] + lambda_1[0, 52] + lambda_1[0, 53] + lambda_1[0, 54] + lambda_1[0, 55] + lambda_1[0, 56] + lambda_1[0, 57] + lambda_1[0, 58] + lambda_1[0, 59] + lambda_1[0, 60] + lambda_1[0, 61] + lambda_1[0, 62] + lambda_1[0, 63] + lambda_1[0, 64] + lambda_1[0, 65] + lambda_1[0, 66] + lambda_1[0, 67] + lambda_1[0, 68] + lambda_1[0, 69]  ==  c[0, 0] ]
+	constraints += [ -lambda_1[0, 1] + lambda_1[0, 2] - 2*lambda_1[0, 5] + 2*lambda_1[0, 6] - 3*lambda_1[0, 9] + 3*lambda_1[0, 10] - 4*lambda_1[0, 13] + 4*lambda_1[0, 14] - lambda_1[0, 18] + lambda_1[0, 19] - lambda_1[0, 20] + lambda_1[0, 21] - lambda_1[0, 23] + lambda_1[0, 24] - 2*lambda_1[0, 25] + 2*lambda_1[0, 26] - lambda_1[0, 27] + lambda_1[0, 28] - 2*lambda_1[0, 29] + 2*lambda_1[0, 30] - lambda_1[0, 32] + lambda_1[0, 33] - 2*lambda_1[0, 35] + 2*lambda_1[0, 36] - 3*lambda_1[0, 37] + 3*lambda_1[0, 38] - lambda_1[0, 39] + lambda_1[0, 40] - 3*lambda_1[0, 41] + 3*lambda_1[0, 42] - lambda_1[0, 44] + lambda_1[0, 45] - 2*lambda_1[0, 48] + 2*lambda_1[0, 49] - 2*lambda_1[0, 50] + 2*lambda_1[0, 51] - lambda_1[0, 55] + lambda_1[0, 56] - lambda_1[0, 57] + lambda_1[0, 58] - lambda_1[0, 60] + lambda_1[0, 61] - 2*lambda_1[0, 62] + 2*lambda_1[0, 63] - lambda_1[0, 64] + lambda_1[0, 65] - lambda_1[0, 67] + lambda_1[0, 68]  ==  c[0, 1] ]
+	constraints += [ lambda_1[0, 5] + lambda_1[0, 6] + 3*lambda_1[0, 9] + 3*lambda_1[0, 10] + 6*lambda_1[0, 13] + 6*lambda_1[0, 14] - lambda_1[0, 17] - lambda_1[0, 23] - lambda_1[0, 24] + lambda_1[0, 25] + lambda_1[0, 26] + lambda_1[0, 29] + lambda_1[0, 30] + 3*lambda_1[0, 37] + 3*lambda_1[0, 38] + 3*lambda_1[0, 41] + 3*lambda_1[0, 42] - 2*lambda_1[0, 47] + lambda_1[0, 48] + lambda_1[0, 49] + lambda_1[0, 50] + lambda_1[0, 51] - lambda_1[0, 53] - lambda_1[0, 54] - lambda_1[0, 57] - lambda_1[0, 58] - lambda_1[0, 59] - lambda_1[0, 60] - lambda_1[0, 61] + lambda_1[0, 62] + lambda_1[0, 63] - lambda_1[0, 66] - lambda_1[0, 69] + 0.1  ==  c[0, 3] ]
+	constraints += [ -lambda_1[0, 3] + lambda_1[0, 4] - 2*lambda_1[0, 7] + 2*lambda_1[0, 8] - 3*lambda_1[0, 11] + 3*lambda_1[0, 12] - 4*lambda_1[0, 15] + 4*lambda_1[0, 16] - lambda_1[0, 18] - lambda_1[0, 19] + lambda_1[0, 20] + lambda_1[0, 21] - lambda_1[0, 25] - lambda_1[0, 26] - 2*lambda_1[0, 27] - 2*lambda_1[0, 28] + lambda_1[0, 29] + lambda_1[0, 30] - lambda_1[0, 31] + 2*lambda_1[0, 32] + 2*lambda_1[0, 33] + lambda_1[0, 34] - lambda_1[0, 37] - lambda_1[0, 38] - 3*lambda_1[0, 39] - 3*lambda_1[0, 40] + lambda_1[0, 41] + lambda_1[0, 42] - 2*lambda_1[0, 43] + 3*lambda_1[0, 44] + 3*lambda_1[0, 45] + 2*lambda_1[0, 46] - 2*lambda_1[0, 48] - 2*lambda_1[0, 49] + 2*lambda_1[0, 50] + 2*lambda_1[0, 51] - lambda_1[0, 53] + lambda_1[0, 54] - lambda_1[0, 57] - lambda_1[0, 58] - 2*lambda_1[0, 59] + lambda_1[0, 60] + lambda_1[0, 61] - lambda_1[0, 64] - lambda_1[0, 65] + 2*lambda_1[0, 66] + lambda_1[0, 67] + lambda_1[0, 68]  ==  c[0, 2] ]
+	constraints += [ lambda_1[0, 18] - lambda_1[0, 19] - lambda_1[0, 20] + lambda_1[0, 21] + 2*lambda_1[0, 25] - 2*lambda_1[0, 26] + 2*lambda_1[0, 27] - 2*lambda_1[0, 28] - 2*lambda_1[0, 29] + 2*lambda_1[0, 30] - 2*lambda_1[0, 32] + 2*lambda_1[0, 33] + 3*lambda_1[0, 37] - 3*lambda_1[0, 38] + 3*lambda_1[0, 39] - 3*lambda_1[0, 40] - 3*lambda_1[0, 41] + 3*lambda_1[0, 42] - 3*lambda_1[0, 44] + 3*lambda_1[0, 45] + 4*lambda_1[0, 48] - 4*lambda_1[0, 49] - 4*lambda_1[0, 50] + 4*lambda_1[0, 51] + lambda_1[0, 57] - lambda_1[0, 58] - lambda_1[0, 60] + lambda_1[0, 61] + lambda_1[0, 64] - lambda_1[0, 65] - lambda_1[0, 67] + lambda_1[0, 68]  ==  c[0, 5] ]
+	constraints += [ lambda_1[0, 7] + lambda_1[0, 8] + 3*lambda_1[0, 11] + 3*lambda_1[0, 12] + 6*lambda_1[0, 15] + 6*lambda_1[0, 16] - lambda_1[0, 22] + lambda_1[0, 27] + lambda_1[0, 28] - lambda_1[0, 31] + lambda_1[0, 32] + lambda_1[0, 33] - lambda_1[0, 34] + 3*lambda_1[0, 39] + 3*lambda_1[0, 40] + 3*lambda_1[0, 44] + 3*lambda_1[0, 45] + lambda_1[0, 48] + lambda_1[0, 49] + lambda_1[0, 50] + lambda_1[0, 51] - 2*lambda_1[0, 52] - lambda_1[0, 55] - lambda_1[0, 56] + lambda_1[0, 59] - lambda_1[0, 62] - lambda_1[0, 63] - lambda_1[0, 64] - lambda_1[0, 65] + lambda_1[0, 66] - lambda_1[0, 67] - lambda_1[0, 68] - lambda_1[0, 69] + 0.1  ==  c[0, 4] ]
+		
+	constraints += [ -lambda_2[0, 0] - lambda_2[0, 1] - lambda_2[0, 2] - lambda_2[0, 3] - lambda_2[0, 4] - lambda_2[0, 5] - lambda_2[0, 6] - lambda_2[0, 7] - lambda_2[0, 8] - lambda_2[0, 9] - lambda_2[0, 10] - lambda_2[0, 11] - lambda_2[0, 12] - lambda_2[0, 13] - lambda_2[0, 14] - lambda_2[0, 15] - lambda_2[0, 16] - lambda_2[0, 17] - lambda_2[0, 18] - lambda_2[0, 19] - lambda_2[0, 20] - lambda_2[0, 21] - lambda_2[0, 22] - lambda_2[0, 23] - lambda_2[0, 24] - lambda_2[0, 25] - lambda_2[0, 26] - lambda_2[0, 27] - lambda_2[0, 28] - lambda_2[0, 29] - lambda_2[0, 30] - lambda_2[0, 31] - lambda_2[0, 32] - lambda_2[0, 33] - lambda_2[0, 34] - lambda_2[0, 35] - lambda_2[0, 36] - lambda_2[0, 37] - lambda_2[0, 38] - lambda_2[0, 39] - lambda_2[0, 40] - lambda_2[0, 41] - lambda_2[0, 42] - lambda_2[0, 43] - lambda_2[0, 44] - lambda_2[0, 45] - lambda_2[0, 46] - lambda_2[0, 47] - lambda_2[0, 48] - lambda_2[0, 49] - lambda_2[0, 50] - lambda_2[0, 51] - lambda_2[0, 52] - lambda_2[0, 53] - lambda_2[0, 54] - lambda_2[0, 55] - lambda_2[0, 56] - lambda_2[0, 57] - lambda_2[0, 58] - lambda_2[0, 59] - lambda_2[0, 60] - lambda_2[0, 61] - lambda_2[0, 62] - lambda_2[0, 63] - lambda_2[0, 64] - lambda_2[0, 65] - lambda_2[0, 66] - lambda_2[0, 67] - lambda_2[0, 68] - lambda_2[0, 69]  ==  0 ]
+	constraints += [ lambda_2[0, 1] - lambda_2[0, 2] + 2*lambda_2[0, 5] - 2*lambda_2[0, 6] + 3*lambda_2[0, 9] - 3*lambda_2[0, 10] + 4*lambda_2[0, 13] - 4*lambda_2[0, 14] + lambda_2[0, 18] - lambda_2[0, 19] + lambda_2[0, 20] - lambda_2[0, 21] + lambda_2[0, 23] - lambda_2[0, 24] + 2*lambda_2[0, 25] - 2*lambda_2[0, 26] + lambda_2[0, 27] - lambda_2[0, 28] + 2*lambda_2[0, 29] - 2*lambda_2[0, 30] + lambda_2[0, 32] - lambda_2[0, 33] + 2*lambda_2[0, 35] - 2*lambda_2[0, 36] + 3*lambda_2[0, 37] - 3*lambda_2[0, 38] + lambda_2[0, 39] - lambda_2[0, 40] + 3*lambda_2[0, 41] - 3*lambda_2[0, 42] + lambda_2[0, 44] - lambda_2[0, 45] + 2*lambda_2[0, 48] - 2*lambda_2[0, 49] + 2*lambda_2[0, 50] - 2*lambda_2[0, 51] + lambda_2[0, 55] - lambda_2[0, 56] + lambda_2[0, 57] - lambda_2[0, 58] + lambda_2[0, 60] - lambda_2[0, 61] + 2*lambda_2[0, 62] - 2*lambda_2[0, 63] + lambda_2[0, 64] - lambda_2[0, 65] + lambda_2[0, 67] - lambda_2[0, 68]  ==  -c[0, 1] + c[0, 2] ]
+	constraints += [ -lambda_2[0, 5] - lambda_2[0, 6] - 3*lambda_2[0, 9] - 3*lambda_2[0, 10] - 6*lambda_2[0, 13] - 6*lambda_2[0, 14] + lambda_2[0, 17] + lambda_2[0, 23] + lambda_2[0, 24] - lambda_2[0, 25] - lambda_2[0, 26] - lambda_2[0, 29] - lambda_2[0, 30] - 3*lambda_2[0, 37] - 3*lambda_2[0, 38] - 3*lambda_2[0, 41] - 3*lambda_2[0, 42] + 2*lambda_2[0, 47] - lambda_2[0, 48] - lambda_2[0, 49] - lambda_2[0, 50] - lambda_2[0, 51] + lambda_2[0, 53] + lambda_2[0, 54] + lambda_2[0, 57] + lambda_2[0, 58] + lambda_2[0, 59] + lambda_2[0, 60] + lambda_2[0, 61] - lambda_2[0, 62] - lambda_2[0, 63] + lambda_2[0, 66] + lambda_2[0, 69] - 0.2  ==  -2*c[0, 3] + c[0, 5] ]
+	constraints += [ lambda_2[0, 9] - lambda_2[0, 10] + 4*lambda_2[0, 13] - 4*lambda_2[0, 14] - lambda_2[0, 23] + lambda_2[0, 24] - 2*lambda_2[0, 35] + 2*lambda_2[0, 36] + lambda_2[0, 37] - lambda_2[0, 38] + lambda_2[0, 41] - lambda_2[0, 42] - lambda_2[0, 57] + lambda_2[0, 58] - lambda_2[0, 60] + lambda_2[0, 61]  ==  0 ]
+	constraints += [ -lambda_2[0, 13] - lambda_2[0, 14] + lambda_2[0, 35] + lambda_2[0, 36] - lambda_2[0, 47]  ==  0 ]
+	constraints += [ lambda_2[0, 3] - lambda_2[0, 4] + 2*lambda_2[0, 7] - 2*lambda_2[0, 8] + 3*lambda_2[0, 11] - 3*lambda_2[0, 12] + 4*lambda_2[0, 15] - 4*lambda_2[0, 16] + lambda_2[0, 18] + lambda_2[0, 19] - lambda_2[0, 20] - lambda_2[0, 21] + lambda_2[0, 25] + lambda_2[0, 26] + 2*lambda_2[0, 27] + 2*lambda_2[0, 28] - lambda_2[0, 29] - lambda_2[0, 30] + lambda_2[0, 31] - 2*lambda_2[0, 32] - 2*lambda_2[0, 33] - lambda_2[0, 34] + lambda_2[0, 37] + lambda_2[0, 38] + 3*lambda_2[0, 39] + 3*lambda_2[0, 40] - lambda_2[0, 41] - lambda_2[0, 42] + 2*lambda_2[0, 43] - 3*lambda_2[0, 44] - 3*lambda_2[0, 45] - 2*lambda_2[0, 46] + 2*lambda_2[0, 48] + 2*lambda_2[0, 49] - 2*lambda_2[0, 50] - 2*lambda_2[0, 51] + lambda_2[0, 53] - lambda_2[0, 54] + lambda_2[0, 57] + lambda_2[0, 58] + 2*lambda_2[0, 59] - lambda_2[0, 60] - lambda_2[0, 61] + lambda_2[0, 64] + lambda_2[0, 65] - 2*lambda_2[0, 66] - lambda_2[0, 67] - lambda_2[0, 68]  ==  -c[0, 1] ]
+	constraints += [ -lambda_2[0, 18] + lambda_2[0, 19] + lambda_2[0, 20] - lambda_2[0, 21] - 2*lambda_2[0, 25] + 2*lambda_2[0, 26] - 2*lambda_2[0, 27] + 2*lambda_2[0, 28] + 2*lambda_2[0, 29] - 2*lambda_2[0, 30] + 2*lambda_2[0, 32] - 2*lambda_2[0, 33] - 3*lambda_2[0, 37] + 3*lambda_2[0, 38] - 3*lambda_2[0, 39] + 3*lambda_2[0, 40] + 3*lambda_2[0, 41] - 3*lambda_2[0, 42] + 3*lambda_2[0, 44] - 3*lambda_2[0, 45] - 4*lambda_2[0, 48] + 4*lambda_2[0, 49] + 4*lambda_2[0, 50] - 4*lambda_2[0, 51] - lambda_2[0, 57] + lambda_2[0, 58] + lambda_2[0, 60] - lambda_2[0, 61] - lambda_2[0, 64] + lambda_2[0, 65] + lambda_2[0, 67] - lambda_2[0, 68]  ==  -2*c[0, 3] + 2*c[0, 4] - c[0, 5] ]
+	constraints += [ lambda_2[0, 25] + lambda_2[0, 26] - lambda_2[0, 29] - lambda_2[0, 30] + 3*lambda_2[0, 37] + 3*lambda_2[0, 38] - 3*lambda_2[0, 41] - 3*lambda_2[0, 42] + 2*lambda_2[0, 48] + 2*lambda_2[0, 49] - 2*lambda_2[0, 50] - 2*lambda_2[0, 51] - lambda_2[0, 53] + lambda_2[0, 54] - lambda_2[0, 57] - lambda_2[0, 58] - 2*lambda_2[0, 59] + lambda_2[0, 60] + lambda_2[0, 61] + 2*lambda_2[0, 66]  ==  0 ]
+	constraints += [ -lambda_2[0, 37] + lambda_2[0, 38] + lambda_2[0, 41] - lambda_2[0, 42] + lambda_2[0, 57] - lambda_2[0, 58] - lambda_2[0, 60] + lambda_2[0, 61]  ==  0 ]
+	constraints += [ -lambda_2[0, 7] - lambda_2[0, 8] - 3*lambda_2[0, 11] - 3*lambda_2[0, 12] - 6*lambda_2[0, 15] - 6*lambda_2[0, 16] + lambda_2[0, 22] - lambda_2[0, 27] - lambda_2[0, 28] + lambda_2[0, 31] - lambda_2[0, 32] - lambda_2[0, 33] + lambda_2[0, 34] - 3*lambda_2[0, 39] - 3*lambda_2[0, 40] - 3*lambda_2[0, 44] - 3*lambda_2[0, 45] - lambda_2[0, 48] - lambda_2[0, 49] - lambda_2[0, 50] - lambda_2[0, 51] + 2*lambda_2[0, 52] + lambda_2[0, 55] + lambda_2[0, 56] - lambda_2[0, 59] + lambda_2[0, 62] + lambda_2[0, 63] + lambda_2[0, 64] + lambda_2[0, 65] - lambda_2[0, 66] + lambda_2[0, 67] + lambda_2[0, 68] + lambda_2[0, 69]  ==  -c[0, 5] ]
+	constraints += [ lambda_2[0, 27] - lambda_2[0, 28] + lambda_2[0, 32] - lambda_2[0, 33] + 3*lambda_2[0, 39] - 3*lambda_2[0, 40] + 3*lambda_2[0, 44] - 3*lambda_2[0, 45] + 2*lambda_2[0, 48] - 2*lambda_2[0, 49] + 2*lambda_2[0, 50] - 2*lambda_2[0, 51] - lambda_2[0, 55] + lambda_2[0, 56] - 2*lambda_2[0, 62] + 2*lambda_2[0, 63] - lambda_2[0, 64] + lambda_2[0, 65] - lambda_2[0, 67] + lambda_2[0, 68]  ==  0 ]
+	constraints += [ -lambda_2[0, 48] - lambda_2[0, 49] - lambda_2[0, 50] - lambda_2[0, 51] + lambda_2[0, 59] + lambda_2[0, 62] + lambda_2[0, 63] + lambda_2[0, 66] - lambda_2[0, 69]  ==  0 ]
+	constraints += [ lambda_2[0, 11] - lambda_2[0, 12] + 4*lambda_2[0, 15] - 4*lambda_2[0, 16] - lambda_2[0, 31] + lambda_2[0, 34] + lambda_2[0, 39] + lambda_2[0, 40] - 2*lambda_2[0, 43] - lambda_2[0, 44] - lambda_2[0, 45] + 2*lambda_2[0, 46] - lambda_2[0, 64] - lambda_2[0, 65] + lambda_2[0, 67] + lambda_2[0, 68]  ==  -c[0, 2] ]
+	constraints += [ -lambda_2[0, 39] + lambda_2[0, 40] + lambda_2[0, 44] - lambda_2[0, 45] + lambda_2[0, 64] - lambda_2[0, 65] - lambda_2[0, 67] + lambda_2[0, 68]  ==  -c[0, 5] ]
+	constraints += [ -lambda_2[0, 15] - lambda_2[0, 16] + lambda_2[0, 43] + lambda_2[0, 46] - lambda_2[0, 52] - 0.2  ==  -2*c[0, 4] ]
 
-	constraints = Lyapunov_func_encoding(deg, Poly, dynamics, X, alpha, max_deg, poly_list, monomial_list)
+	# constraints = Lyapunov_func_encoding(deg, Poly, dynamics, X, alpha, max_deg, poly_list, monomial_list)
 	problem = cp.Problem(objective, constraints)
 	assert problem.is_dcp()
 	assert problem.is_dpp()
@@ -151,7 +182,11 @@ X = [x, y]
 # X_bar = [x_bar, y_bar, z_bar]
 dynamics = [-x**3+y, -x-y]
 Poly=[x+1, 1-x, y+1, 1-y]
-t = FindLyapunov(2, Poly, dynamics, X, 0.1, 4)
+# _ = Lyapunov_func_encoding(2, Poly, dynamics, X, 0.1, 4)
+
+
+t = FindLyapunov(2, Poly, X, 4)
+
 print(t)
 
 
