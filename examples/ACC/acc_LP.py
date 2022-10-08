33da45327130266d7cc5c07ac9eb367257ec3058
SimonZhan-code <simonzhan@berkeley.edu>
2022-10-08 15:38:42 -0700

    some updates

--- examples/ACC/acc_LP.py
+++ examples/ACC/acc_LP.py
@@ -8,6 +8,10 @@ import matplotlib.pyplot as plt
 from sympy import MatrixSymbol, Matrix
 from sympy import *
 import numpy.linalg as LA
+from itertools import *
+import matplotlib.patches as mpatches
+from handelman_utils import *
+from timer import *
 
 class ACC:
 	deltaT = 0.1
@@ -50,6 +54,7 @@ class ACC:
 		# similar to tracking or stablizing to origin point design
 		reward = -(x_l_new - x_e_new - 10 - 1.4 * v_e_new)**2 - (v_l_new - v_e_new)**2 - (r_l_new - r_e_new)**2 
 		return self.state, reward, self.t == self.max_iteration
+
 
 def SVG(control_param, view=False):
 	np.set_printoptions(precision=2)
@@ -98,14 +103,6 @@ def SVG(control_param, view=False):
 		x_l, v_l, r_l, x_e, v_e, r_e = state_tra[i]
 		a_e = control_tra[i]
 
-		# rs = np.array([
-		# 	-x_l / distance_tra[i], 
-		# 	-v_l / distance_tra[i], 
-		# 	-r_l / distance_tra[i], 
-		# 	-x_e / distance_tra[i],
-		# 	-v_e / distance_tra[i],
-		# 	-r_e / distance_tra[i]
-		# 	])
 
 		rs = np.array([
 			-2*(x_l - x_e - 10 - 1.4 * v_e), 
@@ -142,6 +139,97 @@ def SVG(control_param, view=False):
 		vtheta_prime = vtheta
 
 	return vtheta, state
+
+
+def BarrierConstraints():
+
+	def generateConstraints(exp1, exp2, file, degree):
+		for x in range(degree+1):
+			for y in range(degree+1):
+				for z in range(degree+1):
+					for m in range(degree+1):
+
+						for n in range(degree+1):
+							for p in range(degree+1):
+								if x + y + z + m + n + p <= degree:
+									if exp1.coeff(x_l,x).coeff(v_l,y).coeff(r_l,z).coeff(x_e,m).coeff(v_e,n).coeff(r_e,p) != 0:
+										if exp2.coeff(x_l,x).coeff(v_l,y).coeff(r_l,z).coeff(x_e,m).coeff(v_e,n).coeff(r_e,p) != 0:
+											file.write('constraints += [' + str(exp1.coeff(x_l,x).coeff(v_l,y).coeff(r_l,z).coeff(x_e,m).coeff(v_e,n).coeff(r_e,p)) + ' >= ' + str(exp2.coeff(x_l,x).coeff(v_l,y).coeff(r_l,z).coeff(x_e,m).coeff(v_e,n).coeff(r_e,p)) + '- objc' + ']\n')
+											file.write('constraints += [' + str(exp1.coeff(x_l,x).coeff(v_l,y).coeff(r_l,z).coeff(x_e,m).coeff(v_e,n).coeff(r_e,p)) + ' <= ' + str(exp2.coeff(x_l,x).coeff(v_l,y).coeff(r_l,z).coeff(x_e,m).coeff(v_e,n).coeff(r_e,p)) + '+ objc' + ']\n')
+												# print('constraints += [', exp1.coeff(x,a).coeff(y,b).coeff(z,c).coeff(m,d).coeff(n,e).coeff(p,f), ' == ', exp2.coeff(x,a).coeff(y,b).coeff(z,c).coeff(m,d).coeff(n,e).coeff(p,f), ']')
+										else:
+											file.write('constraints += [' + str(exp1.coeff(x_l,x).coeff(v_l,y).coeff(r_l,z).coeff(x_e,m).coeff(v_e,n).coeff(r_e,p)) + ' == ' + str(exp2.coeff(x_l,x).coeff(v_l,y).coeff(r_l,z).coeff(x_e,m).coeff(v_e,n).coeff(r_e,p)) + ']\n')
+
+	x_l, v_l, r_l, x_e, v_e, r_e, theta = symbols('x_l, v_l, r_l, x_e, v_e, r_e')
+	# Confined in the [-2,2]^6 spaces
+	initial_set = [x_l-90, 92-x_l, v_l-20, 30-v_l, v_e-30, 30.5-v_e, x_e-30, 31-x_e, r_l, r_e]
+	X = [x_l, v_l, r_l, x_e, v_e, r_e]
+	# print("setting up")
+	# Generate the possible handelman product to the power defined
+	init_poly_list = Matrix(possible_handelman_generation(2, initial_set))
+	# print("generating poly_list")
+	# incorporate the interval with handelman basis
+	monomial = monomial_generation(2, X)
+	# monomial.remove(1)
+	monomial_list = Matrix(monomial)
+	# print("generating monomial terms")
+	# print(monomial_list)
+	V = MatrixSymbol('V', 1, len(monomial_list))
+	lambda_poly_init = MatrixSymbol('lambda_1', 1, len(init_poly_list))
+
+	lhs_init = V * monomial_list
+	# lhs_init = V * monomial_list
+	lhs_init = lhs_init[0, 0].expand()
+	# print("Get done the left hand side mul")
+	
+	rhs_init = lambda_poly_init * init_poly_list
+	# print("Get done the right hand side mul")
+	rhs_init = rhs_init[0, 0].expand()
+	file = open("barrier.txt","w")
+	file.write("#-------------------The Initial Set Conditions-------------------\n")
+	generateConstraints(rhs_init, lhs_init, file, degree=2)
+		# f.close()
+	
+
+	u0Base = Matrix([[x_l - x_e - 1.4 * v_e, v_l - v_e, r_l - r_e]])
+	t0 = MatrixSymbol('t0', 1, 3)
+	a_e = t0*u0Base.T
+	
+	dynamics = [v_l, 
+				r_l, 
+				-2*r_l-25*sin(v_l)-0.0001*v_l**2, 
+				v_e, 
+				r_l,  
+				-2*r_e+2*a_e-0.0001*v_e**2]
+	# lhs_der= -gradVtox*dynamics - n*Matrix([2 - a**2 - b**2 - c**2 - d**2 - e**2 - f**2])
+	# lhs_der = expand(lhs_der[0, 0])
+	l = 0.2
+	temp = monomial_generation(2, X)
+	monomial_der = GetDerivative(dynamics, temp, X)
+	lhs_der = V * monomial_der - l*V*monomial_list - theta
+	lhs_der = lhs_der[0,0].expand()
+
+	lie_poly_list = [x_l-10-2.4*x_e, 50000-x_l, 50000-x_e, 100-v_l, 100-v_e, 10-r_l, 10-r_e]
+	lie_poly = Matrix(possible_handelman_generation(2, lie_poly_list))
+	lambda_poly_der = MatrixSymbol('lambda_1', 1, len(init_poly_list))
+	rhs_der = lambda_poly_der * lie_poly
+	rhs_der = rhs_der[0,0].expand()
+
+	# with open('cons.txt', 'a+') as f:
+	file.write("\n")
+	file.write("#------------------The Lie Derivative conditions------------------\n")
+	generateConstraints(rhs_der, lhs_der, file, degree=4)
+	file.write("\n")
+	file.write("#------------------Monomial and Polynomial Terms------------------\n")
+	file.write("polynomial terms:"+str(monomial_list)+"\n")
+	file.write("number of polynomial terms:"+str(len(monomial_list))+"\n")
+	file.write(str(len(monomial))+"\n")
+	file.write("\n")
+	file.write("#------------------Lie Derivative test------------------\n")
+	temp = V*monomial_der
+	file.write(str(expand(temp[0, 0]))+"\n")
+	file.close()
+
 
 if __name__ == '__main__':
 	# env = Quadrotor()
